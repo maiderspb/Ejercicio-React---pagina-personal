5b375f90de2dac0343b163df9f05046ae24589d4
Maider San Pedro <maiderspb90@yahoo.es>
2025-07-05 11:29:40 +0200

    Ejercicio React - página personal Checked

--- My personal website/src/components/Navbar.jsx
+++ My personal website/src/components/Navbar.jsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
-    <nav style={{ padding: '1rem', background: '#eee' }}>
-      <Link to="/" style={{ margin: '0 1rem' }}>Home</Link>
-      <Link to="/contact" style={{ margin: '0 1rem' }}>Contact</Link>
-      <Link to="/about" style={{ margin: '0 1rem' }}>About</Link>
+    <nav style={{ padding: "1rem", background: "#eee" }}>
+      <Link to="/" style={{ margin: "0 1rem" }}>
+        Home
+      </Link>
+      <Link to="/contact" style={{ margin: "0 1rem" }}>
+        Contact
+      </Link>
+      <Link to="/about" style={{ margin: "0 1rem" }}>
+        About
+      </Link>
     </nav>
   );
 }
